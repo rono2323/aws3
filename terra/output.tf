e3105e91ce2d54a2e0a9bae900f5b479db9b3462
rono2323 <rohanmahajan716@gmail.com>
2026-08-17 11:05:52 +0530

    Delete all files

--- terra/output.tf
+++
@@ -1,7 +0,0 @@
-output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
-}
-
-output "db_endpoint" {
-  value = aws_db_instance.mysql.endpoint
-}
--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "3tierappaws"  # replace with your bucket name
-    key    = "backend/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
